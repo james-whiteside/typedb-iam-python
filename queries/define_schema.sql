efe537498ca89339177fe0e8edd90b9d7ef5fda2
James Whiteside <117453030+james-whiteside@users.noreply.github.com>
2023-01-24 12:23:26 +0000

    Fix: Restructured the SQL schema.

--- queries/define_schema.sql
+++ queries/define_schema.sql
@@ -1,61 +1,71 @@
 CREATE TABLE users (
-    email_address VARCHAR(256),
-    first_name VARCHAR(256),
     id SERIAL PRIMARY KEY,
-    last_name VARCHAR(256)
+    first_name VARCHAR(256),
+    last_name VARCHAR(256),
+    email_address VARCHAR(256)
+);
+
+CREATE TABLE user_group (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(256),
+    owner_id INT REFERENCES users(id)
+);
+
+CREATE TABLE user_group_hierarchy (
+    parent_user_group INT REFERENCES user_group(id),
+    child_user_group INT REFERENCES user_group(id)
+);
+
+CREATE TABLE user_group_membership (
+    group_id INT REFERENCES user_group(id),
+    user_id INT REFERENCES users(id)
 );
 
 CREATE TABLE business_unit (
-    business_owner INT REFERENCES users(id),
     id SERIAL PRIMARY KEY,
-    name VARCHAR(256)
+    name VARCHAR(256),
+    business_owner INT REFERENCES users(id)
 );
-
-ALTER TABLE users
-ADD COLUMN business_unit_id INT REFERENCES business_unit(id);
 
 CREATE TABLE business_unit_hierarchy (
     parent_business_unit INT REFERENCES business_unit(id),
     child_business_unit INT REFERENCES business_unit(id)
 );
 
-CREATE TABLE roleset (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(256)
-);
+ALTER TABLE users
+ADD COLUMN business_unit_id INT REFERENCES business_unit(id);
 
-CREATE TABLE roleset_hierarchy (
-    parent_roleset INT REFERENCES roleset(id),
-    child_roleset INT REFERENCES roleset(id)
-);
-
-CREATE TABLE role (
+CREATE TABLE user_role (
     id SERIAL PRIMARY KEY,
     name VARCHAR(256),
-    owner_id INT REFERENCES users(id),
-    roleset_id INT REFERENCES roleset(id)
+    owner_id INT REFERENCES users(id)
+);
+
+CREATE TABLE user_role_hierarchy (
+    parent_user_role INT REFERENCES user_role(id),
+    child_user_role INT REFERENCES user_role(id)
 );
 
 CREATE TABLE user_role_membership (
-    role_id INT REFERENCES role(id),
+    user_role_id INT REFERENCES user_role(id),
     user_id INT REFERENCES users(id)
 );
 
-CREATE TABLE account (
+CREATE TABLE user_account (
     id SERIAL PRIMARY KEY,
     name VARCHAR(256)
 );
 
 CREATE TABLE user_account_mapping (
-    account_id INT REFERENCES account(id),
+    user_account_id INT REFERENCES user_account(id),
     user_id INT REFERENCES users(id)
 );
 
 CREATE TABLE directory (
-    business_owner INT REFERENCES users(id),
-    business_unit_id INT REFERENCES business_unit(id),
     id SERIAL PRIMARY KEY,
-    name VARCHAR(256)
+    name VARCHAR(256),
+    business_unit_id INT REFERENCES business_unit(id),
+    business_owner INT REFERENCES users(id)
 );
 
 CREATE TABLE directory_hierarchy (
@@ -64,9 +74,9 @@ CREATE TABLE directory_hierarchy (
 );
 
 CREATE TABLE directory_resource (
-    directory_id INT REFERENCES directory(id),
     id SERIAL PRIMARY KEY,
     name VARCHAR(256),
+    directory_id INT REFERENCES directory(id),
     owner_id INT REFERENCES users(id)
 );
 
@@ -86,16 +96,16 @@ CREATE TABLE directory_entitlement (
 );
 
 CREATE TABLE user_direct_entitlement (
-    entitlement_id INT REFERENCES entitlement(id),
-    user_id INT REFERENCES users(id)
+    user_id INT REFERENCES users(id),
+    entitlement_id INT REFERENCES entitlement(id)
 );
 
-CREATE TABLE role_entitlement (
-    entitlement_id INT REFERENCES entitlement(id),
-    role_id INT REFERENCES role(id)
+CREATE TABLE user_role_entitlement (
+    user_role_id INT REFERENCES user_role(id),
+    entitlement_id INT REFERENCES entitlement(id)
 );
 
-CREATE TABLE account_entitlement (
-    account_id INT REFERENCES account(id),
+CREATE TABLE user_account_entitlement (
+    user_account_id INT REFERENCES user_account(id),
     entitlement_id INT REFERENCES entitlement(id)
 );
